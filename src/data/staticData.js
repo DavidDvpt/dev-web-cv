e7773fc5bdd125c6a113c4cdf74242623d9e6bbc
David MOSCA <david.mosca69@gmail.com>
2020-11-24 18:42:19 +0100

    add formation datas

---
+++ src/data/staticData.js
@@ -0,0 +1,32 @@
+export const formationsAndDiplomes = [
+    {
+        date: 2020,
+        nom: "Développeur web et mobile",
+        ecole: "Wild Code School",
+        lieu: "Biarritz (64)",
+    },
+    {
+        date: 2013,
+        nom: "Développeur logiciel",
+        ecole: "AFPA",
+        lieu: "Pont-de-Claix (38)",
+    },
+    {
+        date: 1994,
+        nom: "Formation technique pour chef de service",
+        ecole: "ITECH, LT Diderot",
+        lieu: "Lyon (69)",
+    },
+    {
+        date: 1991,
+        nom: "BTS Textile option Tissage",
+        ecole: "LT Diderot",
+        lieu: "Lyon (69)",
+    },
+    {
+        date: 1989,
+        nom: "Bac F3 (électrotechnique)",
+        ecole: "LT Diderot",
+        lieu: "Lyon (69)",
+    },
+];
