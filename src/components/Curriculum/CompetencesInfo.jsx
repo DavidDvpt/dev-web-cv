1e36d0e326f50e5f371ffd45e66a24ef868896e4
David MOSCA <david.mosca69@gmail.com>
2020-12-04 17:33:58 +0100

    change css on competences informatiques

--- src/components/Curriculum/CompetencesInfo.jsx
+++ src/components/Curriculum/CompetencesInfo.jsx
@@ -23,67 +23,67 @@ function CompetencesInfo() {
                             alt="profil"
                             className="profilIcon"
                         />{" "}
-                        Comptences informatiques
+                        Compétences informatiques
                     </h4>
                 </Col>
             </Row>
             <Row>
-                <Col className="col-3">Hard skill</Col>
+                <Col className="col-3 my-auto">Hard skill</Col>
                 <Col className="col-9">
                     <Row>
                         <FontAwesomeIcon
                             icon={faNodeJs}
                             size="2x"
-                            className="col my-2"
+                            className="m-2"
                             color="#88c043"
                         />
                         <FontAwesomeIcon
                             icon={faJs}
                             size="2x"
-                            className="col my-2"
+                            className="m-2"
                             color="#f7df1e"
                         />
                         <FontAwesomeIcon
                             icon={faReact}
                             size="2x"
-                            className="col my-2"
+                            className="m-2"
                             color="	#61dafb"
                         />
                         <FontAwesomeIcon
                             icon={faPhp}
                             size="2x"
-                            className="col my-2"
+                            className="m-2"
                             color="#8993c1"
                         />
                         <FontAwesomeIcon
                             icon={faDatabase}
                             size="2x"
-                            className="col my-2"
+                            className="m-2"
                             color="#ffa500"
                         />
                         <FontAwesomeIcon
                             icon={faBootstrap}
                             size="2x"
-                            className="col my-2"
+                            className="m-2"
                             color="#563d7c"
                         />
                     </Row>
                 </Col>
             </Row>
             <Row>
-                <Col className="col-3">Environnement</Col>
+                <Col className="col-3 my-auto">Environnement</Col>
                 <Col className="col-9">
                     <Row>
                         <FontAwesomeIcon
                             icon={faGitAlt}
                             size="2x"
-                            className="col-2 my-2"
+                            className="m-2"
                             color="#f05033"
                         />
                         <FontAwesomeIcon
                             icon={faUbuntu}
                             size="2x"
-                            className="col-2 my-2"
+                            className="m-2"
                             color="#ff5e09"
                         />
                     </Row>
