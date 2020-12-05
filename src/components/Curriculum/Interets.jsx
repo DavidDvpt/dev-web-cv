c4977f5c071265a774ce5306997d10b898a4b4de
David MOSCA <david.mosca69@gmail.com>
2020-12-05 16:10:55 +0100

    add football + css in row

--- src/components/Curriculum/Interets.jsx
+++ src/components/Curriculum/Interets.jsx
@@ -3,6 +3,7 @@ import {
     faGamepad,
     faLaptopCode,
     faUmbrellaBeach,
+    faFutbol,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Col, Row } from "reactstrap";
@@ -11,48 +12,60 @@ import hobby from "../../images/hobby.png";
 
 function Interets() {
     return (
-        <Row>
-            <Col>
-                <h4 className="title titleColLeft col-10">
-                    <img src={hobby} alt="profil" className="profilIcon" />{" "}
-                    Intérets
-                </h4>
-            </Col>
-
-            <Col className="col-12">
-                <Row className="mb-2">
-                    <Col className="col-1">
+        <>
+            <Row>
+                <Col>
+                    <h4 className="title titleColLeft col-10">
+                        <img src={hobby} alt="profil" className="profilIcon" />{" "}
+                        Intérets
+                    </h4>
+                </Col>
+            </Row>
+            <Row className="p-0 mb-3">
+                <Col className="p-0">
+                    <div className="text-center">
                         <FontAwesomeIcon icon={faGamepad} size="2x" />
-                    </Col>
-                    <p className="col-11 pl-4">
-                        Entropia Universe, DaoC, jeux d'aventures (Tomb Raider,
-                        Uncharted, ...)
+                    </div>
+                    <p className="col-11 p-0 m-0">
+                        jeux d'aventures (Tomb Raider, Uncharted) et surtout
+                        mmorpg (Entropia Universe, DAoC)
                     </p>
-                </Row>
-                <Row className="mb-2">
-                    <Col className="col-1">
+                </Col>
+                <Col className="p-0 ">
+                    <div className="text-center">
                         <FontAwesomeIcon icon={faFilm} size="2x" />
-                    </Col>
-                    <p className="col-11 pl-4">
-                        films et séries comme Lords of Rings, GoT, TWD
+                    </div>
+                    <p className="col-11 p-0 m-0">
+                        fantastique, heroic fantaisy, policier. GoT, TWD, The
+                        Black List.
                     </p>
-                </Row>
-                <Row className="mb-2">
-                    <Col className="col-1">
+                </Col>
+            </Row>
+            <Row>
+                <Col className="p-0">
+                    <div className="text-center">
                         <FontAwesomeIcon icon={faUmbrellaBeach} size="2x" />
-                    </Col>
-                    <p className="col-11 pl-4">la côte basque</p>
-                </Row>
-                <Row className="mb-2">
-                    <Col className="col-1">
+                    </div>
+                    <p className="col-11 p-0 m-0">la côte basque</p>
+                </Col>
+                <Col className="p-0">
+                    <div className="text-center">
                         <FontAwesomeIcon icon={faLaptopCode} size="2x" />
-                    </Col>
-                    <p className="col-11 pl-4">
-                        l'informatique et tout ce qui gravite autour
+                    </div>
+                    <p className="col-11 p-0 m-0">
+                        l'informatique hardware, software, programmation
                     </p>
-                </Row>
-            </Col>
-        </Row>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="p-0 col-6">
+                    <div className="text-center">
+                        <FontAwesomeIcon icon={faFutbol} size="2x" />
+                    </div>
+                    <p className="col-11 p-0 m-0">Olympique Lyonnais</p>
+                </Col>
+            </Row>
+        </>
     );
 }
 
