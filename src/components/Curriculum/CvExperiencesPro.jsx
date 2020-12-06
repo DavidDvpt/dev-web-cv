92e435206da3ecf5de5952922ceb1c0e4ff2b8d7
David MOSCA <david.mosca69@gmail.com>
2020-12-06 11:42:33 +0100

    icon changed on comp info and exp prof

--- src/components/Curriculum/CvExperiencesPro.jsx
+++ src/components/Curriculum/CvExperiencesPro.jsx
@@ -2,14 +2,14 @@ import { Col, Row } from "reactstrap";
 import { experiencesProfessionnelles } from "../../data/staticData";
 import CvExperiencePro from "./CvExperiencePro";
 
-import question from "../../images/question.png";
+import bureau from "../../images/bureau.png";
 
 function CvExperiencesPro({ className }) {
     return (
         <Row className={className}>
             <Col>
                 <h4 className="title col-10">
-                    <img src={question} alt="profil" className="profilIcon" />{" "}
+                    <img src={bureau} alt="profil" className="profilIcon" />{" "}
                     Expérience professionnelle
                 </h4>
             </Col>
